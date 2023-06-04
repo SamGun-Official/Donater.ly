2ac1d2010717c6d266530a8f5a61fe35cc7cf155
Clarissa Gracienne <clarissagracia.cg@gmail.com>
2023-06-04 10:40:10 +0700

    Update profile_screen.dart

--- lib/donater_screen/profile_screen.dart
+++ lib/donater_screen/profile_screen.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:multiplatform_donation_app/donater_screen/edit_profile_screen.dart';
-import 'package:multiplatform_donation_app/login_page_screen.dart';
 import 'package:multiplatform_donation_app/menu_page_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
